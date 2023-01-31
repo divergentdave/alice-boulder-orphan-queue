d5aa176aa76f0ba254b0080d24421f6cb3876f1f
David Cook <divergentdave@gmail.com>
2023-01-30 21:06:16 -0600

    Store verifier logs for review

--- run_checker.sh
+++ run_checker.sh
@@ -7,9 +7,12 @@ export ALICE_HOME="$TOPSRCDIR/alice"
 PATH="$PATH:$TOPSRCDIR/alice/bin"
 source "$TOPSRCDIR/.venv/bin/activate"
 
+rm -rf checker_logs
+
 # Build
 (cd harness/verifier; go build .)
 
 # Check possible execution traces with the verifier.
 alice-check --traces_dir=traces_dir \
+    --log_dir=checker_logs \
     --checker=./harness/verifier/verifier
